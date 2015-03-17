97d1e1acb08e3cf3ebbe2b2df430df7c8d4f94a4
michaelmfd <m.boehm@mfd-diagnostics.com>
2015-03-17 11:42:52 +0100

    Add a Exit Button

---
+++ Auto Fluorescence Analyser/NadhjaWinform/NadhjaForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NadhjaWinform
+{
+    public partial class NadhjaForm : Form
+    {
+        /// <summary>
+        /// Constructor: NadhjaForm
+        /// Description: Initialize all winform controlls.
+        /// </summary>
+        public NadhjaForm()
+        {
+            //Initialize all forms
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
